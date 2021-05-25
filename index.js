0671e4e8712ecaee40ec78ff72890e61cde697c9
Alessio Italo Junior D'Alise <Alessioitalo@gmail.com>
2021-05-25 16:55:02 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    
+    const toggleSwitch = document.querySelector('input[type="checkbox"]')
+    const monthlyPrices = document.querySelectorAll('.monthly')
+    const annualPrices = document.querySelectorAll('.yearly')
+
+    toggleSwitch.addEventListener('change', ()=>{
+        if (toggleSwitch.checked){
+            monthlyPrices.forEach((monthlyPrice)=>{
+                monthlyPrice.classList.add('visible')
+                monthlyPrice.classList.remove('hidden')
+            annualPrices.forEach((annualPrice)=>{
+                annualPrice.classList.add('hidden')
+                annualPrice.classList.add('visible')
+            })})
+        }else{
+            annualPrices.forEach((annualPrice)=>{
+                annualPrice.classList.add('visible')
+                annualPrice.classList.remove('hidden')
+            monthlyPrices.forEach((monthlyPrice)=>{
+                monthlyPrice.classList.add('hidden')
+                monthlyPrice.classList.remove('visible')
+            })
+                // annualPrice.classList.toggle('hidden')
+                console.log(annualPrice)
+           })
+        }
+    })
+});
